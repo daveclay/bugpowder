5edba68a24a4c6e359d2e695c3a96b4a2e9d7d21
Dave Clay <David.Clay@IGT.com>
2013-04-11 08:20:17 -0700

    more power

--- src/main/webapp/js/bugpowder.js
+++ src/main/webapp/js/bugpowder.js
@@ -3,11 +3,27 @@ $(document).ready(function() {
     fear.load();
 });
 
+function Image(element, index) {
+    this.element = element;
+    this.index = index;
+}
+
+Image.prototype.show = function() {
+    this.element.removeClass("hidden");
+};
+
+Image.prototype.hide = function() {
+    this.element.addClass("hidden");
+};
+
 function Fear() {
+    this.randomer = new Randomer();
+    this.timer = new Timer();
+    this.timer.delay = 70;
     this.container = $('<div/>');
     this.container.addClass("container");
     addToBody(this.container);
-    this.index = 0;
+    this.currentlyVisibleImage = 0;
     this.images = [];
 }
 
@@ -20,36 +36,87 @@ Fear.prototype.load = function() {
 
 Fear.prototype.handleImages = function(images) {
     var self = this;
-    images.forEach(function(image) {
-        self.handleImage(image);
+    images.forEach(function(image, idx) {
+        self.handleImage(image, idx);
     });
 
+    this.timer.length = this.images.length;
+    this.timer.onUpdate(function(tick) {
+        self.nextImage(tick);
+    });
+    this.chosenImage = this.images[this.randomer.randomInt(this.images.length)];
     this.startSequence();
 };
 
-Fear.prototype.handleImage = function(imageUrl) {
+Fear.prototype.handleImage = function(imageUrl, idx) {
     var img = $('<img/>');
     img.addClass("fearImg hidden");
     img.attr("src", imageUrl);
-    var image = {};
-    image.element = img;
+    var image = new Image(img, idx);
     this.images.push(image);
     this.container.append(img);
 };
 
-Fear.prototype.nextImage = function(image) {
-    this.images[this.index].element.addClass("hidden");
-    this.index++;
-    if (this.index == this.images.length) {
-        this.index = 0;
+Fear.prototype.nextImage = function(tick) {
+    if (this.randomer.moreOftenThan(90)) {
+        this.show(this.chosenImage);
+    } else if (this.randomer.moreOftenThan(80)) {
+    } else {
+        this.show(this.images[tick]);
     }
-    this.images[this.index].element.removeClass("hidden");
 };
 
-Fear.prototype.startSequence = function(image) {
-    var self = this;
-    setInterval(function() {
-        self.nextImage();
-    }, 100);
+Fear.prototype.show = function(image) {
+    if (this.currentlyVisibleImage) {
+        this.currentlyVisibleImage.hide();
+    }
+    image.show();
+    this.currentlyVisibleImage = image;
 };
+
+Fear.prototype.startSequence = function() {
+    this.timer.start();
+};
+
+function Randomer() {
+}
+
+Randomer.prototype.moreOftenThan = function(percent) {
+    return this.randomInt(100) > percent;
+};
+
+Randomer.prototype.randomInt = function(range) {
+    return Math.round(Math.random() * range);
+};
+
+function Timer() {
+    this.tick = 0;
+    this.length = 100;
+    this.delay = 100;
+    this.onUpdateHandler = function() {};
+}
+
+Timer.prototype.onUpdate = function(handler) {
+    this.onUpdateHandler = handler;
+};
+
+Timer.prototype.start = function() {
+    var self = this;
+    this.interval = setInterval(function() {
+        self.update();
+    }, this.delay);
+};
+
+Timer.prototype.update = function() {
+    this.onUpdateHandler(this.tick);
+    this.tick++;
+    if (this.tick >= this.length) {
+        this.tick = 0;
+    }
+};
+
+Timer.prototype.stop = function() {
+    clearInterval(this.interval);
+};
+
 
