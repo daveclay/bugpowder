de79e49b56e25d2b49798d24014ae65337ef757e
Nate Bowen <nate@natebowen.com>
2013-04-15 11:11:14 -0700

    Moving SampleStream into a separate file.

--- src/main/scala/net/retorx/audio/SpeechSplitter.scala
+++ src/main/scala/net/retorx/audio/SpeechSplitter.scala
@@ -1,16 +1,10 @@
 package net.retorx.audio
 
-import java.io.BufferedInputStream
 import java.io.FileInputStream
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.util.Arrays
 import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
 import java.io.DataOutputStream
-import java.io.FileOutputStream
-import java.io.OutputStream
 import java.io.InputStream
 import scala.util.Random
 import java.net.URL
@@ -41,7 +35,7 @@ class SpeechSplitter(inputStream:InputStream, fileNameBase:String) {
 
 		try {
 			val samplesOfSilence = (sampleStream.sampleRate * secondsOfSilence)
-			println("Waiting for " + secondsOfSilence + "s of silence, which is " + samplesOfSilence + " frames at " + sampleStream.sampleRate + " Hz")
+			println("Waiting for " + secondsOfSilence + "s of silence, which is " + samplesOfSilence + " samples at " + sampleStream.sampleRate + " Hz")
 			
 			var sampleList : List[Short] = null
 			do {
@@ -113,67 +107,6 @@ class SpeechSplitter(inputStream:InputStream, fileNameBase:String) {
       loudSamples = 0
       writingToFile = false
 
-	}
-}
-
-class SampleStream(inputStream : InputStream) {
-	val originalAIS = AudioSystem.getAudioInputStream(new BufferedInputStream(inputStream))
-	val originalFormat = originalAIS.getFormat
-	println("Original format is: " + originalFormat)
-	
-	val decodedFormat = new AudioFormat(
-			AudioFormat.Encoding.PCM_SIGNED,
-			originalFormat.getSampleRate,
-			16,
-			originalFormat.getChannels,
-			originalFormat.getChannels * 2,
-			originalFormat.getSampleRate,
-			false
-	    )
-	
-	val decodedAIS = AudioSystem.getAudioInputStream(decodedFormat, originalAIS)
-
-	val bytesPerFrame = decodedFormat.getFrameSize
-	println("bytesPerFrame: " + bytesPerFrame)
-
-	val framesPerRead = 1024
-
-	val buf = new Array[Byte](framesPerRead * bytesPerFrame)
-	var byteBuf : ByteBuffer = null
-
-	var bytesInCurrentBuf = -1
-	var posInCurrentBuf = -1
-	
-	def sampleRate = decodedFormat.getSampleRate
-	def channels = decodedFormat.getChannels
-			
-	def nextSample : List[Short] = {
-	  if (posInCurrentBuf == -1 || posInCurrentBuf >= bytesInCurrentBuf) {
-		bytesInCurrentBuf = decodedAIS.read(buf)
-		if (bytesInCurrentBuf > 0) {
-			posInCurrentBuf = 0;
-			byteBuf = ByteBuffer.wrap(buf,0,bytesInCurrentBuf)
-			if ( ! decodedFormat.isBigEndian ) {
-				byteBuf.order(ByteOrder.LITTLE_ENDIAN)
-			}
-		} else {
-		  return List[Short]()
-		}
-	  }
-	  
-	  val sampleList = new Array[Short](decodedFormat.getChannels)
-	  for (i <- 0 until decodedFormat.getChannels) {
-	    try {
-		    sampleList(i) = byteBuf.getShort
-			posInCurrentBuf += 2 // I dunno, we've hard-coded 16 around, I'm confused.
-	    } catch {
-	      case e : Exception => {
-	        List[Short]()
-	      }
-	    }
-	  }
-	  
-	  List.fromArray(sampleList)
 	}
 }
 
