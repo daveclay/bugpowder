69a3bd10b0172b1eebd8023691829801177a40d2
Nate Bowen <nate@natebowen.com>
2013-04-15 00:24:34 -0700

    Sort of interim commit -- created SampleStream but haven yet to test or use it.

--- src/main/scala/net/retorx/audio/SpeechSplitter.scala
+++ src/main/scala/net/retorx/audio/SpeechSplitter.scala
@@ -158,9 +158,56 @@ class SpeechSplitter(inputStream:InputStream, fileNameBase:String) {
       writingToFile = false
 
 	}
+}
+
+class SampleStream(inputStream : InputStream) {
+	val originalAIS = AudioSystem.getAudioInputStream(new BufferedInputStream(inputStream))
+	val originalFormat = originalAIS.getFormat
+	println("Original format is: " + originalFormat)
 	
-  
-  
+	val decodedFormat = new AudioFormat(
+			AudioFormat.Encoding.PCM_SIGNED,
+			originalFormat.getSampleRate,
+			16,
+			originalFormat.getChannels,
+			originalFormat.getChannels * 2,
+			originalFormat.getSampleRate,
+			false
+	    )
+	
+	val decodedAIS = AudioSystem.getAudioInputStream(decodedFormat, originalAIS)
+
+	val bytesPerFrame = decodedFormat.getFrameSize
+	println("bytesPerFrame: " + bytesPerFrame)
+
+	val framesPerRead = 1024
+
+	val buf = new Array[Byte](framesPerRead * bytesPerFrame)
+	var byteBuf : ByteBuffer = null
+
+	var bytesInCurrentBuf = -1
+	var posInCurrentBuf = -1
+	
+	def sampleRate = decodedFormat.getSampleRate
+			
+	def nextSample : List[Short] = {
+	  if (posInCurrentBuf == -1 || posInCurrentBuf >= bytesInCurrentBuf) {
+		bytesInCurrentBuf = decodedAIS.read(buf)
+		posInCurrentBuf = 0;
+		byteBuf = ByteBuffer.wrap(buf,0,bytesInCurrentBuf)
+		if ( ! decodedFormat.isBigEndian ) {
+			byteBuf.order(ByteOrder.LITTLE_ENDIAN)
+		}
+	  }
+	  
+	  val sampleList = new Array[Short](decodedFormat.getChannels)
+	  for (i <- 0 until decodedFormat.getChannels) {
+		sampleList(i) = byteBuf.getShort
+		posInCurrentBuf += 2 // I dunno, we've hard-coded 16 around, I'm confused.
+	  }
+	  
+	  List.fromArray(sampleList)
+	}
 }
 
 object SpeechSplitter extends App {
