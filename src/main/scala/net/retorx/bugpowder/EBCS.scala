336ce2f10866a56821ed1eaf8e4da3aa358efad7
Nate Bowen <nate@natebowen.com>
2013-04-20 13:31:18 -0700

    Adding VOA and DemocracyNow.

--- src/main/scala/net/retorx/bugpowder/EBCS.scala
+++ src/main/scala/net/retorx/bugpowder/EBCS.scala
@@ -9,6 +9,8 @@ import java.io.FileInputStream
 import scala.xml.XML
 import net.retorx.audio.SpeechSplitter
 import java.net.URL
+import java.util.Calendar
+import java.text.DecimalFormat
 
 @Singleton
 class EBCS {
@@ -18,14 +20,51 @@ class EBCS {
     
     new Thread() {
       override def run() {
+
         val foxNewsMP3URLs =
           (XML.load(new URL("http://feeds.foxnewsradio.com/foxnewsradiocom"))
               \\ "enclosure" \\ "@url").filter( url => url.text.endsWith("3") )
               
         foxNewsMP3URLs.foreach( node => {
-        	val speechSplitter = new SpeechSplitter(new URL(node.text).openStream,"foxnews",0.035,0.125,audioDirectoryPath)
-        	speechSplitter.split()
+            try {
+        	    val speechSplitter = new SpeechSplitter(new URL(node.text).openStream,"foxnews-",0.035,0.125,audioDirectoryPath)
+        	    speechSplitter.split()
+            } catch {
+                case e : Exception => e.printStackTrace()
+            }
         })
+
+        
+        val twodigitformat = new DecimalFormat("00")
+        var cal = Calendar.getInstance()
+
+        for (i <- 0 until 7) {
+            val url = "http://gandalf.ddo.jp/mp3/" + cal.get(Calendar.YEAR).toString.substring(2) + twodigitformat.format(cal.get(Calendar.MONTH)) + twodigitformat.format(cal.get(Calendar.DAY_OF_MONTH)) + ".mp3"
+            try {
+        	    val speechSplitter = new SpeechSplitter(new URL(url).openStream,"VOA-",0.035,0.125,audioDirectoryPath)
+        	    speechSplitter.split()
+            } catch {
+                case e : Exception => e.printStackTrace()
+            }
+            cal.roll(Calendar.DAY_OF_MONTH,-1)
+        }
+        
+        cal = Calendar.getInstance()
+        var successfullyProcessed = 0
+        var attempted = 0
+        while (successfullyProcessed < 2 && attempted < 7) {
+            attempted += 1
+            val url = "http://traffic.libsyn.com/democracynow/dn" + cal.get(Calendar.YEAR) + "-" + twodigitformat.format(cal.get(Calendar.MONTH)) + twodigitformat.format(cal.get(Calendar.DAY_OF_MONTH)) + "-1.mp3"
+            try {
+        	    val speechSplitter = new SpeechSplitter(new URL(url).openStream,"DemocracyNow-",0.035,0.125,audioDirectoryPath)
+        	    speechSplitter.split()
+        	    successfullyProcessed += 1
+            } catch {
+                case e : Exception => e.printStackTrace()
+            }
+            cal.roll(Calendar.DAY_OF_MONTH,-1)
+        }
+        
       }
     }.start()
 
