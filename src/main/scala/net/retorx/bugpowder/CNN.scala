cb6913793daef278ce0d796b9336152e6d2459fa
daveclay <daveclay@gmail.com>
2013-04-30 13:35:09 -0700

    Refactored the Fear

--- src/main/scala/net/retorx/bugpowder/CNN.scala
+++ src/main/scala/net/retorx/bugpowder/CNN.scala
@@ -1,11 +1,34 @@
 package net.retorx.bugpowder
 
 import com.google.inject.Singleton
-import scala.util.Random
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
 import scala.collection.JavaConversions._
 import scala.collection.immutable.HashSet
+
+@Singleton
+class HuffingtonPost extends FilteringFearSourceImp(
+    Array("http://www.huffingtonpost.com/politics/"),
+    src => src.contains("i.huffington"))
+
+@Singleton
+class Townhall extends FilteringFearSourceImp(
+    Array("http://townhall.com/"),
+    src => src.contains("media") && (! src.contains("comments.png") || ! src.contains("partner"))
+)
+
+@Singleton
+class FoxNews extends FilteringFearSourceImp(
+    Array("http://www.foxnews.com",
+        "http://www.foxnews.com/politics/index.html"),
+    src => (src.contains("root_images") || src.contains("ucat") || src.contains("managed")) && (! src.contains("logo")))
+
+@Singleton
+class CNN extends FilteringFearSourceImp(
+    Array("http://www.cnn.com",
+        "http://www.cnn.com/POLITICS/",
+        "http://politicalticker.blogs.cnn.com/"),
+    src => src.contains("asset"))
+
 
 @Singleton
 class FearBuilder {
@@ -27,12 +50,9 @@ trait FearSource {
     def getImages:List[String]
 }
 
-@Singleton
-class HuffingtonPost extends FearSource {
-    def getImages = {
-        val urls = Array(
-            "http://www.huffingtonpost.com/politics/")
+class FilteringFearSourceImp(urls: Array[String], filter:String => Boolean) extends FearSource {
 
+    def getImages = {
         urls.foldLeft(List[String]())((list,url) => {
             val images = grab(url)
             list ++ images
@@ -44,85 +64,6 @@ class HuffingtonPost extends FearSource {
         val images = doc.select("img")
         images.iterator().map(element => {
             element.attr("src")
-        }).filter(src => {
-            (src.contains("i.huffington"))
-        })
+        }).filter(filter)
     }
-
-}
-
-@Singleton
-class Townhall extends FearSource {
-    def getImages = {
-        val urls = Array(
-            "http://townhall.com/")
-
-        urls.foldLeft(List[String]())((list,url) => {
-            val images = grab(url)
-            list ++ images
-        })
-    }
-
-    private def grab(url: String) = {
-        val doc = Jsoup.connect(url).get
-        val images = doc.select("img")
-        images.iterator().map(element => {
-            element.attr("src")
-        }).filter(src => {
-            (src.contains("media")) && (! src.contains("comments.png") || ! src.contains("partner"))
-        })
-    }
-
-}
-
-@Singleton
-class FoxNews extends FearSource {
-    def getImages = {
-        val urls = Array(
-            "http://www.foxnews.com",
-            "http://www.foxnews.com/politics/index.html")
-
-        urls.foldLeft(List[String]())((list,url) => {
-            val images = grab(url)
-            list ++ images
-        })
-    }
-
-    private def grab(url: String) = {
-        val doc = Jsoup.connect(url).get
-        val images = doc.select("img")
-        images.iterator().map(element => {
-            element.attr("src")
-        }).filter(src => {
-            (src.contains("root_images") || src.contains("ucat") || src.contains("managed")) && (! src.contains("logo"))
-        })
-    }
-
-}
-
-@Singleton
-class CNN extends FearSource {
-
-    def getImages = {
-        val urls = Array(
-            "http://www.cnn.com",
-            "http://www.cnn.com/POLITICS/",
-            "http://politicalticker.blogs.cnn.com/")
-
-        urls.foldLeft(List[String]())((list,url) => {
-            val images = grab(url)
-            list ++ images
-        })
-    }
-
-    private def grab(url: String) = {
-        val doc = Jsoup.connect(url).get
-        val images = doc.select("img")
-        images.iterator().map(element => {
-            element.attr("src")
-        }).filter(src => {
-            src.contains("asset")
-        })
-    }
-
 }
